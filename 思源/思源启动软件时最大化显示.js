e20b2686873159d7c5e3eb70e32db4da7852af70
wish <wishemail@163.com>
2024-11-22 02:20:20 +0000

    add 思源/思源启动软件时最大化显示.js.
    Signed-off-by: wish <wishemail@163.com>

---
+++ 思源/思源启动软件时最大化显示.js
@@ -0,0 +1,26 @@
+// 思源启动软件时最大化显示
+// see https://ld246.com/article/1732239161536
+(()=>{
+    // true,仅在启动软件时最大化，刷新时不最大化; false,刷新页面时也最大化
+    const onlyStart = true;
+
+    if(onlyStart) {
+        const hasMaxmized = sessionStorage.getItem("hasMaxmized") || '';
+        if(hasMaxmized === 'yes') return;
+    }
+    
+    try {
+        if(typeof require === 'function') {
+            const electron = require('electron');
+            if(typeof electron === 'object') {
+                const ipcRenderer = electron.ipcRenderer;
+                if (ipcRenderer) {
+                    ipcRenderer.send('siyuan-cmd', 'maximize');
+                    sessionStorage.setItem("hasMaxmized", "yes");
+                }
+            }
+        }
+    } catch (e) {
+        console.log(e);
+    }
+})();
