97a8838f717adbf847d937f8d120e599b2b16618
wish <wishemail@163.com>
2024-11-27 22:44:24 +0000

    update 思源/思源通过标签插入当前块到数据库SuperTag.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/思源通过标签插入当前块到数据库SuperTag.js
+++ 思源/思源通过标签插入当前块到数据库SuperTag.js
@@ -53,8 +53,8 @@
             block = document.querySelector('div[data-node-id="'+(block?.dataset?.nodeId||'')+'"]');
             if(!block) return;
             // 判断是否在列表元素内，数据库插入列表项
-            const listLeafNode = block.closest('div[data-node-id][data-type="NodeList"]');
-            if(listLeafNode) block = listLeafNode;
+            const leafItemNode = block.closest('div[data-node-id][data-type="NodeListItem"]');
+            if(leafItemNode) block = leafItemNode;
         }
         const blockId = block?.dataset?.nodeId;
         if(!blockId) return;
