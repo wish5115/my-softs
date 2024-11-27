85fe3fd849aa58f6399abecb82e9cf2ec366ace6
wish <wishemail@163.com>
2024-11-27 01:54:41 +0000

    update 思源/思源通过自定义属性播放块音频.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/思源通过自定义属性播放块音频.js
+++ 思源/思源通过自定义属性播放块音频.js
@@ -1,21 +1,38 @@
 // 通过自定义属性播放块音频
-// see https://ld246.com/article/1732606877452
+// version 0.0.2
+// 更新记录
+// 0.0.2 增加了按钮鼠标悬停效果，增加了对自定义emoji的支持
 (async ()=>{
     // 这里定义自定义属性名，这里不需要带custom-，但Markdown代码里必须加custom-前缀，注意属性使用小写，否则思源也会转换为小写
     const attrName = 'attrname';
     const audioFileAttrName = 'audiofilename';
     const startTimeAttrName = 'start';
     const endTimeAttrName = 'end';
+    // 不使用该参数，填空即可
     const playEmoji = '▶️';
+    // 自定义Emoji，格式，path/xxx，无需加图片扩展名，不使用该参数，填空即可
+    // 假设自定义表情路径是，data/emojis/demo/demo.png，则只需要填写：demo/demo 即可。
+    const playEmojiCustom = '';
 
-    observeAudioBlock('custom-' + attrName, block => {
+    observeAudioBlock('custom-' + attrName.replace(/^custom\-/i,''), block => {
         const content = block.firstElementChild;
         if(!content) return;
-        content.innerHTML = content.innerHTML.replace(playEmoji, '<span class="audio-play-btn" style="cursor:pointer">'+playEmoji+'</span>');
-        content.querySelector('.audio-play-btn')?.addEventListener('click', () => {
-            const file = block.getAttribute('custom-' + audioFileAttrName);
-            let startTime = block.getAttribute('custom-' + startTimeAttrName);
-            let endTime = block.getAttribute('custom-' + endTimeAttrName);
+        if(content.querySelector('.audio-play-btn')) return;
+        let customEmoji;
+        if(playEmojiCustom) customEmoji = content.querySelector('img[alt="'+playEmojiCustom.replace(/^:|:$/g,'')+'"]');
+        let audioPlayBtn;
+        if(customEmoji) {
+            customEmoji.classList.add('audio-play-btn');
+            audioPlayBtn = customEmoji;
+        } else {
+            if(!playEmoji) return;
+            content.innerHTML = content.innerHTML.replace(playEmoji, '<span class="audio-play-btn">'+playEmoji+'</span>');
+            audioPlayBtn = content.querySelector('.audio-play-btn');
+        }
+        audioPlayBtn?.addEventListener('click', (event) => {
+            const file = block.getAttribute('custom-' + audioFileAttrName.replace(/^custom\-/i,''));
+            let startTime = block.getAttribute('custom-' + startTimeAttrName.replace(/^custom\-/i,''));
+            let endTime = block.getAttribute('custom-' + endTimeAttrName.replace(/^custom\-/i,''));
             if(!file || !startTime || !endTime) return;
             startTime = parseFloat(startTime) || 0;
             endTime = parseFloat(endTime) || 0;
@@ -23,8 +40,16 @@
                 showErrorMessage('播放失败，开始时间不能大于结束时间');
             } 
             playAudio(file, startTime, endTime);
+            event.stopPropagation();
+            event.preventDefault();
         });
     });
+
+    // 添加样式
+    addStyle(`
+        .audio-play-btn {cursor: pointer;}
+        .audio-play-btn:hover {opacity: 0.8;}
+    `);
 
     var audio, timeUpdateHandler, currentEndTime = 0;
     function playAudio(file, startTime, endTime) {
@@ -55,6 +80,15 @@
             "method": "POST",
             "body": JSON.stringify({"msg": message})
         })
+    }
+    // 添加style标签
+    function addStyle(css) {
+        // 创建一个新的style元素
+        const style = document.createElement('style');
+        // 设置CSS规则
+        style.innerHTML = css;
+        // 将style元素添加到<head>中
+        document.head.appendChild(style);
     }
     // 监听tag被添加
     function observeAudioBlock(attrName, callback) {
