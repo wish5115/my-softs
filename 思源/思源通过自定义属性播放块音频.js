981197a6bffac7bf5a1e207ca9b0d67986fa6bdd
wish <wishemail@163.com>
2024-12-01 15:44:08 +0000

    update 思源/思源通过自定义属性播放块音频.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/思源通过自定义属性播放块音频.js
+++ 思源/思源通过自定义属性播放块音频.js
@@ -13,18 +13,22 @@
     // 块播放按钮自定义Emoji，格式，path/xxx，无需加图片扩展名，不使用该参数，填空即可
     // 假设自定义表情路径是，data/emojis/demo/demo.png，则只需要填写：demo/demo 即可。
     // 如果同一个块中多个相同播放按钮，仅第一个生效
-    const playEmojiCustom = 'your_path/block-play';
+    let playEmojiCustom = 'your_path/block-play';
     // 标题播放按钮自定义Emoji，不使用该参数，填空即可
     //（该功能是通过获取标题下的所有音频的最小开始时间和最大结束时间实现的，因此不通用，如果你的音频不是连续播放的，勿用此功能）
     // 如果同一个标题中多个相同播放按钮，仅第一个生效
-    const playEmojiCustomHead = 'your_path/head-play';
+    let playEmojiCustomHead = 'your_path/head-play';
+
+    // 去除两边:号
+    playEmojiCustom = playEmojiCustom.replace(/^:|:$/g,'');
+    playEmojiCustomHead = playEmojiCustomHead.replace(/^:|:$/g,'');
 
     // 监听块播放按钮被加载或添加
     observeAudioElements('custom-' + audioFileAttrName.replace(/^custom\-/i,''), (block, type) => {
         if(!playEmojiCustom) return;
         const content = block.firstElementChild;
         if(!content) return;
-        const customEmoji = content.querySelector('img[alt="'+playEmojiCustom.replace(/^:|:$/g,'')+'"]');
+        const customEmoji = content.querySelector('img[alt="'+playEmojiCustom+'"]');
         const clickEvent = (event) => {
             event.stopPropagation();
             event.preventDefault();
