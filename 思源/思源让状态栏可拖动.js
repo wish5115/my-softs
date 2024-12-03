6cbd9624fc809f357c5672d31db7a163c09bde0b
wish <wishemail@163.com>
2024-12-03 02:09:06 +0000

    add 思源/思源让状态栏可拖动.js.
    Signed-off-by: wish <wishemail@163.com>

---
+++ 思源/思源让状态栏可拖动.js
@@ -0,0 +1,112 @@
+// 让状态栏可拖动
+// 把代码放到js片段中即可
+//[注意] 如果状态栏position是static样式，请先设置为fixed或使用本脚本默认样式position:fixed,right:42px,bottom:-8px;
+// 调用示例
+//moveableStatus() 或 moveableStatus(document.querySelector('#status'));
+moveableStatus();
+function moveableStatus(status) {
+    let isDragging = false;
+    let isDragged = false;
+    let offsetX, offsetY;
+    let left='0px', top='0px';
+    let width = 0, height = 0;
+    const originStyle = {};
+
+    if(!status) status = document.querySelector('#status');
+
+    // 初始时计算宽高和位置
+    const calcStatusStyle = () => {
+        let style = getComputedStyle(status, null);
+        if(!isDragged) {
+            // 如果静态元素设置为固定元素
+            if(style.position === 'static') {
+                status.style.position = 'fixed';
+                status.style.setProperty('right', '42px', 'important');
+                status.style.bottom = '-8px';
+                style = getComputedStyle(status, null);
+            }
+            // 计算状态栏宽高
+            const marginWidth = parseFloat(style.marginLeft) + parseFloat(style.marginRight);
+            const marginHeight = parseFloat(style.marginTop) + parseFloat(style.marginBottom);
+            width = parseFloat(style.width) + marginWidth;
+            height = parseFloat(style.height) + marginHeight;
+
+            // 记录状态栏初始样式
+            originStyle.position = style.position;
+            originStyle.right = style.right;
+            originStyle.bottom = style.bottom;
+        }
+        // 计算状态栏位置
+        left = window.innerWidth - (parseFloat(style.right) + width) + 'px';
+        top = window.innerHeight - (parseFloat(style.bottom) + height) + 'px';
+    };
+    
+    // 改变窗口大小事件
+    window.addEventListener("resize", (event)=>{
+        if (isDragged) {
+            if(parseFloat(status.style.left) > window.innerWidth) {
+                status.style.left = (window.innerWidth - width) + 'px';
+            }
+            if(parseFloat(status.style.top) > window.innerHeight) {
+                status.style.top = (window.innerHeight - height) + 'px';
+            }
+        } else {
+            calcStatusStyle();
+        }
+    });
+    // 加载时计算
+    setTimeout(calcStatusStyle, 200);
+
+    // 双击恢复状态栏
+    status.addEventListener("dblclick", (event)=>{
+        isDragged = false;
+        status.style.position = originStyle.position;
+        status.style.setProperty('right', originStyle.right, 'important');
+        status.style.bottom = originStyle.bottom;
+        status.style.left = 'auto';
+        status.style.top = 'auto';
+        calcStatusStyle();
+    });
+
+    // 拖动事件
+    const dragHandler = (e) => {
+        if (e.type === 'mousedown') {
+            // 开始拖动
+            if(!isDragged) {
+                isDragged = true;
+                status.style.position = 'absolute';
+                status.style.setProperty('right', 'auto', 'important');
+                status.style.bottom = 'auto';
+                status.style.left = left;
+                status.style.top = top;
+            }
+            isDragging = true;
+            document.removeEventListener('mousemove', dragHandler);
+            document.removeEventListener('mouseup', dragHandler);
+            document.addEventListener('mousemove', dragHandler);
+            document.addEventListener('mouseup', dragHandler);
+            offsetX = e.clientX - status.offsetLeft;
+            offsetY = e.clientY - status.offsetTop;
+        } else if (e.type === 'mousemove' && isDragging) {
+            // 拖动中
+            let x = e.clientX - offsetX;
+            let y = e.clientY - offsetY;
+            //限制不超过窗口大小
+            if(x < 0) x = 0;
+            if(y < 0) y = 0;
+            if(x > window.innerWidth - width) x = window.innerWidth - width;
+            if(y > window.innerHeight - height) y = window.innerHeight - height;
+            // 设置状态栏坐标
+            status.style.left = x + 'px';
+            status.style.top = y + 'px';
+        } else if (e.type === 'mouseup') {
+            //结束拖动
+            isDragging = false;
+            document.removeEventListener('mousemove', dragHandler);
+            document.removeEventListener('mouseup', dragHandler);
+        }
+        e.preventDefault();
+    };
+    status.removeEventListener('mousedown', dragHandler);
+    status.addEventListener('mousedown', dragHandler);
+}
