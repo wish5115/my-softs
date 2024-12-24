d504965dbdf0ce981f3a6d4a911ff55ae2636e35
wish <wishemail@163.com>
2024-12-24 04:03:21 +0000

    update 思源/让选中的块或图片变模糊.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/让选中的块或图片变模糊.js
+++ 思源/让选中的块或图片变模糊.js
@@ -3,7 +3,7 @@
 (()=>{
     // 模糊样式
     const filter = 'blur(6px)';
-    // 模糊期间是否可编辑，默认禁止编辑，如果设为true，块编辑后可能导致模糊效果被保存
+    // 模糊期间是否可编辑，默认禁止编辑，如果设为true，块编辑后会导致模糊效果被保存，如果你刚好要这种效果，可以把这个值设为true
     const editableOnBlur = false;
     document.addEventListener('keydown', (event) => {
         // 检查是否按下了 Ctrl/Meta + Alt + B
@@ -17,19 +17,23 @@
                     // 如果已经应用了 blur 样式，则移除
                     element.style.filter = '';
                     // 恢复编辑状态
-                    const contenteditableEl = element.querySelector('[contenteditable][data-blurred="true"]')||element.closest('[contenteditable][data-blurred="true"]');
-                    if(contenteditableEl) {
-                        contenteditableEl.setAttribute('contenteditable', true);
-                        delete contenteditableEl.dataset.blurred;
+                    if(!editableOnBlur) {
+                        const contenteditableEl = element.querySelector('[contenteditable][data-blurred="true"]')||element.closest('[contenteditable][data-blurred="true"]');
+                        if(contenteditableEl) {
+                            contenteditableEl.setAttribute('contenteditable', true);
+                            delete contenteditableEl.dataset.blurred;
+                        }
                     }
                 } else {
                     // 如果没有应用 blur 样式，则添加
                     element.style.filter = filter;
                     // 禁止编辑
-                    const contenteditableEl = element.querySelector('[contenteditable="true"]')||element.closest('[contenteditable="true"]');
-                    if(contenteditableEl) {
-                        contenteditableEl.setAttribute('contenteditable', false);
-                        contenteditableEl.dataset.blurred = true;
+                    if(!editableOnBlur) {
+                        const contenteditableEl = element.querySelector('[contenteditable="true"]')||element.closest('[contenteditable="true"]');
+                        if(contenteditableEl) {
+                            contenteditableEl.setAttribute('contenteditable', false);
+                            contenteditableEl.dataset.blurred = true;
+                        }
                     }
                 }
             });
