07f6df4071f0d8db649f9e56b9da234c1a5fed51
wish <wishemail@163.com>
2025-01-04 00:59:54 +0000

    rename 思源/snippets_checker.js to 思源/snippets_update_checker.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/snippets_checker.js
+++
@@ -1,5 +0,0 @@
-(async ()=>{
-    if(!window.snippetsChecker) window.snippetsChecker = {};
-    window.snippetsChecker.version = '0.0.1';
-    console.log('snippets_checker');
-})();
---
+++ 思源/snippets_update_checker.js
@@ -0,0 +1,5 @@
+(async ()=>{
+    if(!window.snippetsUpdateChecker) window.snippetsUpdateChecker = {};
+    window.snippetsUpdateChecker.version = '0.0.1';
+    console.log('snippets_update_checker');
+})();
