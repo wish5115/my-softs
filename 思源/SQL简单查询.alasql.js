82504e97eb051ffb22ee53dcdb39d0fd04dce9cc
wish <wishemail@163.com>
2025-01-07 19:40:37 +0000

    add 思源/SQL简单查询.alasql.js.
    Signed-off-by: wish <wishemail@163.com>

---
+++ 思源/SQL简单查询.alasql.js
@@ -0,0 +1,8 @@
+// SQL简单查询 alasql扩展
+{
+    // 动态加载alasql库
+    const src = 'https://cdn.bootcdn.net/ajax/libs/alasql/4.5.0/alasql.min.js';
+    const script = document.createElement('script');
+    script.src = src;
+    document.head.appendChild(script);
+}
