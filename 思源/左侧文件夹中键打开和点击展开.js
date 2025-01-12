f110c1d70f97e53a1818af040defc787b7e2f56e
wish <wishemail@163.com>
2025-01-12 12:37:13 +0000

    add 思源/左侧文件夹中键打开和点击展开.js.
    Signed-off-by: wish <wishemail@163.com>

---
+++ 思源/左侧文件夹中键打开和点击展开.js
@@ -0,0 +1,95 @@
+// 左侧文件夹中键打开和点击展开
+// see https://ld246.com/article/1736401552973
+(()=>{
+    whenElementExist(':is(.file-tree, [data-type="sidebar-file"]) .b3-list.b3-list--background').then((tree) => {
+        //////// pc版 中键打开，单击展开 ///////////
+        if(!isTouchDevice()) {
+            // 绑定鼠标单击
+            tree.addEventListener('click', (event) => {
+                const {toggleBtn} = isTreeFolder(event.target);
+                if(!toggleBtn) return;
+                if (event.target.classList.contains("b3-list-item__text")){
+                    event.stopPropagation();
+                    event.preventDefault();
+                    toggleBtn.click();
+                }
+            }, { capture: true });
+    
+            // 绑定中键单击
+            tree.addEventListener('mousedown', (event) => {
+                if (event.button === 1) {
+                    const {li} = isTreeFolder(event.target);
+                    if(!li) return;
+                    li.click();
+                }
+            });
+        }
+
+        //////// 触屏版 长按打开 点击展开 ///////////
+        if(isTouchDevice()) {
+            let pressTimer;
+
+            // 点击事件
+            function handleTap(event) {
+                const {toggleBtn} = isTreeFolder(event.target);
+                if(!toggleBtn) return;
+                if (event.target.classList.contains("b3-list-item__text")||event.target.classList.contains("b3-list-item__icon")){
+                    event.stopPropagation();
+                    event.preventDefault();
+                    toggleBtn.click();
+                }
+            }
+
+            // 长按事件
+            function handleLongPress(event) {
+                const {li} = isTreeFolder(event.target);
+                if(!li) return;
+                li.click();
+            }
+            
+            tree.addEventListener('touchstart', (event) => {
+                pressTimer = setTimeout(() => {
+                    handleLongPress(event);
+                }, 500);
+            });
+            
+            tree.addEventListener('touchend', (event) => {
+                if (pressTimer) {
+                    clearTimeout(pressTimer);
+                    handleTap(event);
+                }
+            });
+            
+            tree.addEventListener('touchmove', (event) => {
+                if (pressTimer) {
+                    clearTimeout(pressTimer);
+                    pressTimer = null;
+                }
+            });
+        }
+    });
+
+    function isTreeFolder(element) {
+        // 判断目标元素是否是 .sy__file li[data-type="navigation-file"]
+        const li = element.closest('li[data-type="navigation-file"]:not([data-type="navigation-root"])');
+        if(!li) return false;
+        // 非文件夹返回
+        const toggleBtn = li.querySelector(':is(.b3-list-item__toggle--hl,.b3-list-item__toggle):not(.fn__hidden)');
+        if(!toggleBtn) return false;
+        return {li, toggleBtn};
+    }
+
+    function isTouchDevice() {
+        return ("ontouchstart" in window) && navigator.maxTouchPoints > 1;
+    }
+
+    function whenElementExist(selector, node) {
+        return new Promise(resolve => {
+            const check = () => {
+                const el = typeof selector==='function'?selector():(node||document).querySelector(selector);
+                if (el) resolve(el); else requestAnimationFrame(check);
+            };
+            check();
+        });
+    }
+})();
