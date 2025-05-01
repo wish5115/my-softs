e64811719f8da1741269b6eb35c6f3e5fe692bcf
wish <wishemail@163.com>
2025-05-01 02:37:07 +0000

    update 思源/给页面临时设置宽屏.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/给页面临时设置宽屏.js
+++ 思源/给页面临时设置宽屏.js
@@ -2,6 +2,9 @@
 // 兼容编辑器宽度插件和思源自适应宽度，兼容标题宽度
 // 修改改自 https://ld246.com/article/1744597829665
 (()=>{
+    // 是否隐藏侧边栏，true 隐藏 false不隐藏
+    const isHideDock = true;
+    
     // 添加风格
     const styles = {
         "↔️": {tips:'宽屏', style:`width: 100%!important;margin:0 auto;padding-left:20px!important;padding-right:20px!important;.av{font-size: 16px;}`},
@@ -37,22 +40,28 @@
                 setPageStyle(val.style);
                 styleStatus[key] = true;
                 button.setAttribute('aria-label', '取消'+val.tips+'风格');
+                // 隐藏侧边栏
+                toggleDockItems(false);
             } else {
                 // 恢复样式
                 setPageStyle('');
                 styleStatus[key] = false;
                 button.setAttribute('aria-label', '设置'+val.tips+'风格');
+                // 显示侧边栏
+                toggleDockItems(true);
             }
         };
         pin.before(button);
     }
 
+    let lastProtyle = null;
     function setPageStyle(style) {
         const protyle = document.querySelector('[data-type="wnd"].layout__wnd--active .protyle:not(.fn__none)');
         const protyleId = protyle?.dataset?.id;
         if(!protyleId) {
             showMessage('请先选择要设置的编辑器！', true);
         }
+        lastProtyle = protyle;
         const width = extractWidth(style);
         // #layouts div.layout__center div.protyle-content:not([data-fullwidth="true"]) div.protyle-wysiwyg
         style = `#layouts div.layout__center .protyle[data-id="${protyleId}"] div.protyle-content div.protyle-wysiwyg{${style}}`;
@@ -104,5 +113,34 @@
             };
             check();
         });
+    }
+    // 隐藏/显示侧边栏
+    let lastDockTypes = [];
+    function toggleDockItems(show) {
+        const activeDocks = document.querySelectorAll('.dock__item--active');
+        if(show === true) {
+            // 显示侧边栏
+            if (lastDockTypes.length > 0) {
+                const dockTypes = Array.from(activeDocks).map(dock => `[data-type="${dock.dataset.type}"]`);
+                lastDockTypes.forEach(dockType => !dockTypes.includes(dockType) && document.querySelector(dockType)?.click());
+            }
+        } else if(show === false) {
+            // 隐藏侧边栏
+            if (activeDocks.length > 0) {
+                lastDockTypes = Array.from(activeDocks).map(dock => `[data-type="${dock.dataset.type}"]`);
+                lastDockTypes.forEach(dockType => document.querySelector(dockType)?.click());
+            } else {
+                lastDockTypes = [];
+            }
+        } else {
+            // 自动触发
+            if (activeDocks.length > 0) {
+                lastDockTypes = Array.from(activeDocks).map(dock => `[data-type="${dock.dataset.type}"]`);
+                lastDockTypes.forEach(dockType => document.querySelector(dockType)?.click());
+            } else if (lastDockTypes.length > 0) {
+                lastDockTypes.forEach(dockType => document.querySelector(dockType)?.click());
+            }
+        }
+        lastProtyle?.querySelector('.protyle-wysiwyg')?.focus();
     }
 })();
