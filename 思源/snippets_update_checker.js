5f258b7b0a078f0c8e31b48c8e4961929308b37a
wish <wishemail@163.com>
2025-05-02 17:29:09 +0000

    update 思源/snippets_update_checker.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/snippets_update_checker.js
+++ 思源/snippets_update_checker.js
@@ -1,3 +1,6 @@
+// name snippets update checker
+// version 0.0.1
+// updateUrl 
 (async ()=>{
     if(!window.snippetsUpdateChecker) window.snippetsUpdateChecker = {};
     window.snippetsUpdateChecker.version = '0.0.1';
