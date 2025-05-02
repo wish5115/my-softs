badbd055f450abed05c432f13c7d9cc491a4b85a
wish <wishemail@163.com>
2025-05-02 17:29:37 +0000

    update 思源/snippets_update_checker.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/snippets_update_checker.js
+++ 思源/snippets_update_checker.js
@@ -1,6 +1,6 @@
 // name snippets update checker
 // version 0.0.1
-// updateUrl 
+// updateUrl https://gitee.com/wish163/mysoft/raw/main/%E6%80%9D%E6%BA%90/snippets_update_checker.js
 (async ()=>{
     if(!window.snippetsUpdateChecker) window.snippetsUpdateChecker = {};
     window.snippetsUpdateChecker.version = '0.0.1';
