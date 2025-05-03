38a4cb8ecd1e8df8d0159c0fa9350a85011517e8
wish <wishemail@163.com>
2025-05-03 16:39:48 +0000

    update 思源/snippets_new_version_checker_for_dev_cdn.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/snippets_new_version_checker_for_dev_cdn.js
+++ 思源/snippets_new_version_checker_for_dev_cdn.js
@@ -1,4 +1,5 @@
+// 思源代码片段自动检查更新（开发者推广计划）
 const script = document.createElement('script');
 //script.src = 'https://raw.githubusercontent.com/wish5115/my-softs/refs/heads/main/%E6%80%9D%E6%BA%90/snippets_new_version_checker_for_dev.js';
-script.src = '';
+script.src = 'https://update.gf.qytechs.cn/scripts/534829/checkNewVersion.user.js';
 document.head.appendChild(script);
