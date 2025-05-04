85069fc18baa8aa5ecf2b018f7198e34fe0f4b3a
wish <wishemail@163.com>
2025-05-04 03:22:42 +0000

    update 思源/snippets_new_version_checker_for_dev_cdn.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/snippets_new_version_checker_for_dev_cdn.js
+++ 思源/snippets_new_version_checker_for_dev_cdn.js
@@ -3,6 +3,7 @@
     const script = document.createElement('script');
     //script.src = 'https://raw.githubusercontent.com/wish5115/my-softs/refs/heads/main/%E6%80%9D%E6%BA%90/snippets_new_version_checker_for_dev.js';
     //script.src = 'https://update.gf.qytechs.cn/scripts/534829/checkNewVersion.user.js';
+    //script.src = 'https://gcore.jsdelivr.net/gh/wish5115/my-softs@main/%E6%80%9D%E6%BA%90/snippets_new_version_checker_for_dev.js';
     //script.src = 'https://fastly.jsdelivr.net/gh/wish5115/my-softs@main/%E6%80%9D%E6%BA%90/snippets_new_version_checker_for_dev.js';
     //script.src = 'https://cdn.jsdmirror.com/gh/wish5115/my-softs@main/%E6%80%9D%E6%BA%90/snippets_new_version_checker_for_dev.js';
     script.src = 'https://cdn.mengze.vip/gh/wish5115/my-softs@main/%E6%80%9D%E6%BA%90/snippets_new_version_checker_for_dev.js';
