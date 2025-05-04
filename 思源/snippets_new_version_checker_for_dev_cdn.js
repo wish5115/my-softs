24a77bea8ebb352e47933bd5da6be6405812144a
wish <wishemail@163.com>
2025-05-04 23:27:42 +0000

    update 思源/snippets_new_version_checker_for_dev_cdn.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/snippets_new_version_checker_for_dev_cdn.js
+++ 思源/snippets_new_version_checker_for_dev_cdn.js
@@ -3,6 +3,7 @@
 // updateUrl https://gitee.com/wish163/mysoft/raw/main/%E6%80%9D%E6%BA%90/snippets_new_version_checker_for_dev_cdn.js
 // udateDesc 增加cdn调整cdn顺序
 (() => {
+    if(window?.snippetsNewVersions?.newVersionLoader || window?.snippetsNewVersions?.setInterval) return;
     const urls = ['jsd.onmicrosoft.cn','cdn.jsdmirror.com','gcore.jsdelivr.net','fastly.jsdelivr.net','quantil.jsdelivr.net','originfastly.jsdelivr.net','cdn.mengze.vip','update.gf.qytechs.cn','jsd.nmmsl.top','cdn.bili33.top','jsdelivr.qaq.qa','gitee.com','cdn.jsdmirror.cn','raw.githubusercontent.com'];
     function loadScript(index) {
         if (index >= urls.length) { console.warn('所有 CDN 节点都无法加载目标脚本。'); return;}
