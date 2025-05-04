6b794ab3e1b8f23589ec904e65407ef95b8658a2
wish <wishemail@163.com>
2025-05-04 22:04:33 +0000

    update 思源/snippets_new_version_checker_for_dev_cdn.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/snippets_new_version_checker_for_dev_cdn.js
+++ 思源/snippets_new_version_checker_for_dev_cdn.js
@@ -1,6 +1,7 @@
 // name 代码片段自动检查更新(CDN)（开发者推广计划）
-// version 0.0.1
+// version 0.0.2
 // updateUrl https://gitee.com/wish163/mysoft/raw/main/%E6%80%9D%E6%BA%90/snippets_new_version_checker_for_dev_cdn.js
+// udateDesc 增加cdn调整cdn顺序
 (() => {
     const urls = ['jsd.onmicrosoft.cn','cdn.jsdmirror.com','gcore.jsdelivr.net','fastly.jsdelivr.net','quantil.jsdelivr.net','originfastly.jsdelivr.net','cdn.mengze.vip','update.gf.qytechs.cn','jsd.nmmsl.top','cdn.bili33.top','jsdelivr.qaq.qa','gitee.com','cdn.jsdmirror.cn','raw.githubusercontent.com'];
     function loadScript(index) {
