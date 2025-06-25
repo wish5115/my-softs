670c22eda373dfea4437fcfd6661602461b3cf9a
wish <wishemail@163.com>
2025-06-25 23:53:55 +0000

    add 思源/通过toolbar按钮开启和关闭块标打开属性面板.js.
    Signed-off-by: wish <wishemail@163.com>

---
+++ 思源/通过toolbar按钮开启和关闭块标打开属性面板.js
@@ -0,0 +1,150 @@
+// 通过toolbar按钮开启和关闭块标打开属性面板
+// see https://ld246.com/article/1750855839822
+(async ()=>{
+    // 如果你的打开面板的快捷键不是ctrl/meta+alt+a，需要配置快捷键
+    // 仅支持 ctrl/meta/alt/shift + 字母按键
+    const openAttrShortcut = '';
+
+    // 是否记住开启状态 true 记住 false 不记住
+    // 不记住时，刷新页面后默认是不开启打开属性面板
+    const isStoreOpenAttrStatus = true;
+
+    // 不支持手机版
+    if(isMobile()) return;
+    
+    // 获取开关状态
+    let openAttrStatus = {isOpenAttr:false};
+    if(isStoreOpenAttrStatus) {
+        openAttrStatus = await getFile('/data/storage/open_attr_status.json') || '{"isOpenAttr":false}';
+        openAttrStatus = JSON.parse(openAttrStatus);
+        if(openAttrStatus.code && openAttrStatus.code !== 0) {
+            openAttrStatus = {isOpenAttr:false};
+        }
+    }
+    
+    // 添加toolbar按钮
+    whenElementExist('#toolbar .fn__ellipsis').then((el)=>{
+        if(!el) return;
+        const ariaLabel = openAttrStatus.isOpenAttr ? '关闭打开属性面板' : '开启打开属性面板';
+        const style = openAttrStatus.isOpenAttr ? 'transform: scaleX(-1);' : '';
+        const html = `<div data-menu="true" id="openAttr" class="toolbar__item ariaLabel" aria-label="${ariaLabel}" data-location="right"><svg style="${style}"><use xlink:href="#iconParagraph"></use></svg></div>`;
+        el.insertAdjacentHTML('afterend', html);
+        const openAttrBtn = el.nextElementSibling;
+        openAttrBtn.addEventListener('click', () => {
+            const svg = openAttrBtn.querySelector("svg");
+            const tooltip = document.querySelector('#tooltip:not(fn__none)');
+            if(!openAttrStatus.isOpenAttr) {
+                // 开启打开属性面板
+                openAttrStatus.isOpenAttr = true;
+                svg.style.transform = "scaleX(-1)";
+                openAttrBtn.setAttribute('aria-label', '关闭打开属性面板');
+                if(tooltip) tooltip.textContent = '关闭打开属性面板';
+            } else {
+                // 关闭打开属性面板
+                openAttrStatus.isOpenAttr = false;
+                svg.style.transform = "";
+                openAttrBtn.setAttribute('aria-label', '开启打开属性面板');
+                if(tooltip) tooltip.textContent = '开启打开属性面板';
+            }
+            // 存储状态
+            if(isStoreOpenAttrStatus) {
+                putFile('/data/storage/open_attr_status.json', JSON.stringify(openAttrStatus));
+            }
+        });
+    });
+    // 监听块标被点击
+    document.addEventListener('click', (e)=> {
+        const gutters = e.target?.closest('.protyle-gutters');
+        if(gutters){
+            // 打开属性面板
+            if(openAttrStatus.isOpenAttr) {
+                event.preventDefault();
+                event.stopPropagation();
+                press(openAttrShortcut||(isMac()?'meta':'ctrl')+'+alt+a', gutters.closest('.protyle')?.querySelector('.protyle-wysiwyg'));
+                // 打开自定义属性
+                whenElementExist('div.b3-dialog--open[data-key="dialog-attr"]').then((dialog) => {
+                    if(dialog) dialog.querySelector('.layout-tab-bar .item--full[data-type="custom"]').click();
+                });
+            }
+        }
+    }, true);
+    function whenElementExist(selector, node = document, timeout = 5000) {
+        return new Promise((resolve, reject) => {
+            const start = Date.now();
+            function check() {
+                let el;
+                try {
+                    el = typeof selector === 'function'
+                        ? selector()
+                        : node.querySelector(selector);
+                } catch (err) {
+                    return resolve(null);
+                }
+                if (el) {
+                    resolve(el);
+                } else if (Date.now() - start >= timeout) {
+                    resolve(null);
+                } else {
+                    requestAnimationFrame(check);
+                }
+            }
+            check();
+        });
+    }
+    // 仅支持 功能键+字母
+    function press(keys = [], element) {
+        if(typeof keys === 'string') keys = keys.split('+');
+        keys = keys.map(item=>item.trim().toLowerCase());
+        const key = keys.find(item=>!['ctrl','alt','meta','shift'].includes(item));
+        const code = `Key${key.toUpperCase()}`;
+        let keyInit = {
+            ctrlKey: keys.includes('ctrl'),
+            altKey: keys.includes('alt'),
+            metaKey: keys.includes('meta'),
+            shiftKey: keys.includes('shift'),
+            key: key.toUpperCase(),
+            keyCode: key.toUpperCase().charCodeAt(0),
+            code: code,
+        }
+        keyInit["bubbles"] = true;
+        let keydownEvent = new KeyboardEvent('keydown', keyInit);
+        if(typeof element === 'string') element = document.querySelector(element);
+        (element || document.getElementsByTagName("body")[0]).dispatchEvent(keydownEvent);
+        let keyUpEvent = new KeyboardEvent('keyup', keyInit);
+        (element || document.getElementsByTagName("body")[0]).dispatchEvent(keyUpEvent);
+    }
+    function isMac() {
+        return navigator.platform.indexOf("Mac") > -1;
+    }
+    function isMobile() {
+        return !!document.getElementById("sidebar");
+    }
+    async function getFile(path) {
+        return fetch("/api/file/getFile", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ path }),
+        }).then((response) => {
+            if (response.ok) {
+                return response.text();
+            } else {
+                throw new Error("Failed to get file content");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+    // 存储文件，支持创建文件夹，当isDir true时创建文件夹，忽略文件
+    async function putFile(path, content = '', isDir = false) {
+        const formData = new FormData();
+        formData.append("path", path);
+        formData.append("isDir", isDir)
+        formData.append("file", new Blob([content]));
+        const result = await fetch("/api/file/putFile", { // 写入js到本地
+            method: "POST",
+            body: formData,
+        });
+        const json = await result.json();
+        return json;
+    }
+})();
