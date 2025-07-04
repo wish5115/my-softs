990a9c152e044f6b7f62e8d94e586876ae7218b9
wish <wishemail@163.com>
2025-07-04 23:59:27 +0000

    update 思源/拦截ctrl+q.js.
    Signed-off-by: wish <wishemail@163.com>

--- 思源/拦截ctrl+q.js
+++ 思源/拦截ctrl+q.js
@@ -1,17 +1,20 @@
 // 拦截ctrl/meta + q 退出事件
-document.addEventListener('keydown', async (e) => {
-    // 判断是否按下了 Ctrl+Q 或 Meta+Q
-    if ((e.ctrlKey || e.metaKey) && e.code === 'KeyQ') {
-        // 阻止事件冒泡和默认行为
-        e.stopPropagation();
-        e.preventDefault();
-        if(window.confirm('即将退出思源，您确定要退出吗？')) {
-            // 内核退出
-            await requestApi('/api/system/exit');
-            // 渲染进程退出
-            exitApp();
+// see https://ld246.com/article/1751671702656
+(()=>{
+    document.addEventListener('keydown', async (e) => {
+        // 判断是否按下了 Ctrl+Q 或 Meta+Q
+        if ((e.ctrlKey || e.metaKey) && e.code === 'KeyQ' && !e.shiftKey && !e.altKey) {
+            // 阻止事件冒泡和默认行为
+            e.stopPropagation();
+            e.preventDefault();
+            if(window.confirm('即将退出思源，您确定要退出吗？')) {
+                // 内核退出
+                await requestApi('/api/system/exit');
+                // 渲染进程退出
+                exitApp();
+            }
         }
-    }
+    }, true);
     async function requestApi(url, data, method = 'POST') {
         return await (await fetch(url, {method: method, body: JSON.stringify(data||{})})).json();
     }
@@ -28,4 +31,4 @@ document.addEventListener('keydown', async (e) => {
             }
         }
     }
-}, true);
+})();
